9b2e46d27b832f6c623eef0792071ff3a895a0ab
GizmoBuyBot <vincentabowman@yahoo.com>
2025-07-08 21:47:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Launching Gizmo BuyBot..."
-
-# Start dummy Flask server to keep Render port open
-python dummy_server.py &
-
-# Start the actual buybot
-python gizmo_buybot.py
+python gizmo_buybot.py &
+python dummy_server.py
